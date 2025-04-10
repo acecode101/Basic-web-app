6e56058ea7ceca4b0d5b9e19a22712beae2cb14c
acecode101 <dharshinibaskaran20@gmail.com>
2025-04-11 00:14:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+from flask import Flask, request, session, redirect
 import os
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
+app = Flask(__name__)
+app.secret_key = 'x'
+
+@app.route('/')
 def home():
-    return "Hello from Render!"
+    return f"Hi {session['u']}! <a href='/lo'>Logout</a>" if 'u' in session else "<form method=post action='/li'><input name=u><input name=p type=password><input type=submit></form>"
+
+@app.route('/li', methods=['POST'])
+def li():
+    if request.form['u'] == 'a' and request.form['p'] == '1': session['u'] = 'a'
+    return redirect('/')
+
+@app.route('/lo')
+def lo():
+    session.pop('u', 0)
+    return redirect('/')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # use PORT env var
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
